d60a06b4bf0736bf752b92ef8b8305b58fa43303
dsgibbons <dsgibbons@tuta.io>
2023-05-31 07:18:19 +0930

    chore: format test

--- tests/plots/test_utils.py
+++ tests/plots/test_utils.py
@@ -6,7 +6,15 @@ from shap.plots import colors
 from shap.plots._utils import convert_color
 
 
-@pytest.mark.parametrize(("color", "expected_result"), [("shap_red", colors.red_rgb), ("shap_blue", colors.blue_rgb), ("null", "null"), ("jet", pl.get_cmap("jet"))])
+@pytest.mark.parametrize(
+    ("color", "expected_result"),
+    [
+        ("shap_red", colors.red_rgb),
+        ("shap_blue", colors.blue_rgb),
+        ("null", "null"),
+        ("jet", pl.get_cmap("jet")),
+    ],
+)
 def test_convert_color(color, expected_result):
     result = convert_color(color)
 
